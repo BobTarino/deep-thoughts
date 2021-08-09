951ced4d3a918f3462cf973926f08e0312d614b7
Bobby T <bob.tarino@gmail.com>
2021-08-09 14:24:26 -0500

    wrapped returning JSX code with <ApolloProvider>

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,34 @@
 import React from 'react';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+
 
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 import Home from './pages/Home';
 
+// establish a link to the GraphQL server at its /graphql endpoint
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3001/graphql',
+});
+
+// use the ApolloClient() constructor to instantiate the Apollo Client instance , create the connection to the API endpoint, and instantiate a new cache object for memory
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <div className='flex-column justify-flex-start min-100-vh'>
-      <Header />
-      <div className='container'>
-        <Home />
+    <ApolloProvider client={client}>
+      <div className="flex-column justify-flex-start min-100-vh">
+        <Header />
+        <div className="container">
+          <Home />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </ApolloProvider>
   );
 }
 
