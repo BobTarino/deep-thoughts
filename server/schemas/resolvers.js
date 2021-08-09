4a90ccd9ef41b4b96fc3d16f1494570f7da95144
Bobby T <bob.tarino@gmail.com>
2021-08-09 10:35:23 -0500

    implemented Auth middleware and populated Me query

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,6 +5,18 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+              const userData = await User.findOne({ _id: context.user._id })
+                .select('-__v -password')
+                .populate('thoughts')
+                .populate('friends');
+          
+              return userData;
+            }
+          
+            throw new AuthenticationError('Not logged in');
+        },
         thoughts: async (parent, { username }) => {
             // use a ternary operator to check if username exists
             const params = username ? { username } : {};
